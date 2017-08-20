cf7b8f9110be32b5f2184e5a689e982eaecd9e79
Guheyhey <joeglancith@hotmail.com>
2017-08-19 21:09:16 -0700

    create some models

---
+++ userfront/src/main/java/com/joegl/domain/User.java
@@ -0,0 +1,130 @@
+package com.joegl.domain;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class User {
+
+    private Long userId;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    private boolean enabled = true;
+
+    private PrimaryAccount primaryAccount;
+
+    private SavingsAccount savingAccount;
+
+    private List<Appointment> appointmentList;
+
+    private List<Recipient> recipientList;
+
+//    private Set<UserRole> userRoles = new HashSet<UserRole>();
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public PrimaryAccount getPrimaryAccount() {
+        return primaryAccount;
+    }
+
+    public void setPrimaryAccount(PrimaryAccount primaryAccount) {
+        this.primaryAccount = primaryAccount;
+    }
+
+    public SavingsAccount getSavingAccount() {
+        return savingAccount;
+    }
+
+    public void setSavingAccount(SavingsAccount savingAccount) {
+        this.savingAccount = savingAccount;
+    }
+
+    public List<Appointment> getAppointmentList() {
+        return appointmentList;
+    }
+
+    public void setAppointmentList(List<Appointment> appointmentList) {
+        this.appointmentList = appointmentList;
+    }
+
+    public List<Recipient> getRecipientList() {
+        return recipientList;
+    }
+
+    public void setRecipientList(List<Recipient> recipientList) {
+        this.recipientList = recipientList;
+    }
+
+//    public Set<UserRole> getUserRoles() {
+//        return userRoles;
+//    }
+//
+//    public void setUserRoles(Set<UserRole> userRoles) {
+//        this.userRoles = userRoles;
+//    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", enabled=" + enabled +
+                ", primaryAccount=" + primaryAccount +
+                ", savingAccount=" + savingAccount +
+                ", appointmentList=" + appointmentList +
+                ", recipientList=" + recipientList +
+                '}';
+    }
+}

97bd5357153fce386cefd0233f6ba8c8432d671a
Guheyhey <joeglancith@hotmail.com>
2017-08-22 20:05:54 -0700

    Import loginService in login.component.ts

--- AdminPortal/src/app/login/login.component.ts
+++ AdminPortal/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable}  from 'rxjs/Observable';
+import {LoginService} from "../login.service";
 
 @Component({
   selector: 'app-login',
